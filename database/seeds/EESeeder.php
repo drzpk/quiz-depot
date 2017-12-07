e5ca3c6853a7ba32f5e0a0dbbf0132f8f9778029
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2017-12-07 19:16:15 +0100

    Aktualizacja seedera - dodanie testów E.12 i E.13

--- database/seeds/EESeeder.php
+++ database/seeds/EESeeder.php
@@ -11,8 +11,8 @@ use App\Library\Database\Model\Question;
  */
 class EESeeder extends Seeder {
 
-    private const TEST_URL = 'http://egzamin-informatyk.pl/e14-egzamin-zawodowy-test-online';
-    private const RESULT_URL = 'http://egzamin-informatyk.pl/e14-odpowiedzi';
+    /** Ilość uruchomień pobierania każdego quizu */
+    private const RUN_COUNT = 10;
 
     private $manager = null;
     private $uploaded = [];
@@ -21,44 +21,67 @@ class EESeeder extends Seeder {
     private $errors = 0;
     private $doubles = 0;
 
-    /** Kategoria egzaminów zawodowych */
-    private $category = null;
-    /** Quiz E.14 */
-    private $quiz = null;
-
     public function run(DatabaseManager $manager) {
         $this->manager = $manager;
 
         // usunięcie starej kopii kategorii, jeśli istnieje
-        $category = $manager->getCategory('inf_exam');
+        $category = $manager->getCategoryByName('Technik informatyk');
         if ($category !== null) {
             $manager->deleteCategory($category);
         }
 
         // utworzenie lub pobranie kategorii
         $category = new Category();
-        $category->name = 'inf_exam';
+        $category->name = 'Technik informatyk';
         $category->description = 'Egzaminy E.12, E.13, E.14';
 
         if (!$manager->createCategory($category)) {
             echo 'Nie udało się utworzyć kategorii: ' . $manager->getLastErrorDesc() . PHP_EOL;
             return;
         }
-        $this->category = $category;
+
+        //E.12
+        $testUrl = 'http://egzamin-informatyk.pl/e12-egzamin-zawodowy-test-online';
+        $resultUrl = 'http://egzamin-informatyk.pl/e12-odpowiedzi';
+        $this->downloadQuiz('E.12', $category, $testUrl, $resultUrl);
+
+        // E.13
+        $testUrl = 'http://egzamin-informatyk.pl/e13-egzamin-zawodowy-test-online';
+        $resultUrl = 'http://egzamin-informatyk.pl/e13-odpowiedzi';
+        $this->downloadQuiz('E.13', $category, $testUrl, $resultUrl);
+
+        // E.14
+        $testUrl = 'http://egzamin-informatyk.pl/e14-egzamin-zawodowy-test-online';
+        $resultUrl = 'http://egzamin-informatyk.pl/e14-odpowiedzi';
+        $this->downloadQuiz('E.14', $category, $testUrl, $resultUrl);
+    }
+
+    private function downloadQuiz($name, $category, $testUrl, $resultUrl) {
+        // reset statystyk
+        $this->sent = 0;
+        $this->errors = 0;
+        $this->doubles;
+        $this->uploaded = [];
+
+        // baner
+        $baner = 'Pobieranie testu: ' . $name;
+        echo str_repeat('-', strlen($baner) + 4) . PHP_EOL;
+        echo "| $baner |" . PHP_EOL;
+        echo str_repeat('-', strlen($baner) + 4) . PHP_EOL . PHP_EOL;
 
         $quiz = new Quiz();
         $quiz->category = $category;
-        $quiz->name = 'Egzamin E.14';
-        if (!$manager->createQuiz($quiz)) {
+        $quiz->name = $name;
+        $quiz->threshold = 20;
+        if (!$this->manager->createQuiz($quiz)) {
             echo 'Nie udało się utworzyć quizu: ' . $manager->getLastErrorDesc() . PHP_EOL;
             return;
         }
-        $this->quiz = $quiz;
 
         // uruchomienie pobierania kilka razy w celu pobranie większej ilości pytań
-        for ($i = 1; $i <= 10; $i++) {
+        for ($i = 1; $i <= self::RUN_COUNT; $i++) {
             echo PHP_EOL . "Pobieranie: $i/10" . PHP_EOL;
-            $this->download();
+            $this->download($quiz, $testUrl, $resultUrl);
         }
 
         echo PHP_EOL . '>> Pobieranie zakończone << ' . PHP_EOL;
@@ -67,10 +90,10 @@ class EESeeder extends Seeder {
         echo "Powtórzonych pytań: $this->doubles" . PHP_EOL;
     }
 
-    private function download() {
+    private function download($quiz, $testUrl, $resultUrl) {
         // wysłanie zapytania w celu pobrania ciasteczek
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, self::TEST_URL);
+        curl_setopt($curl, CURLOPT_URL, $testUrl);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_HEADER, true);
         $response = curl_exec($curl);
@@ -95,7 +118,7 @@ class EESeeder extends Seeder {
 
         // wysłanie drugiego zapytania w celu pobrania pytań i odpowiedzi
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, self::RESULT_URL);
+        curl_setopt($curl, CURLOPT_URL, $resultUrl);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -123,12 +146,12 @@ class EESeeder extends Seeder {
             return false;
         }
 
-        $this->parse($container->item(0));
+        $this->parse($container->item(0), $quiz);
 
         return true;
     }
 
-    private function parse($container) {
+    private function parse($container, $quiz) {
         $index = 1;
         $current_question = null;
         $right_answer = '';
@@ -170,7 +193,7 @@ class EESeeder extends Seeder {
 
                     // rozpoczęcie nowego pytania
                     $current_question = new Question();
-                    $current_question->quiz = $this->quiz;
+                    $current_question->quiz = $quiz;
                     $start_pos = strpos($text, ' ') + 1;
                     $current_question->question = substr($text, $start_pos);
 
